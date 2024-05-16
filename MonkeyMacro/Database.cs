f2e83c74efc05cfbf7971d8b1c49407b51495e73
pms9158@wku.ac.kr <pms9158@wku.ac.kr>
2024-05-17 06:00:47 +0900

    20240517_PMS # 데이터베이스 작업중 - Firestore 패키지 추가 - Database.cs (데이터베이스 연결, 읽기/쓰기) - DataStore.cs (유저 데이터 로딩) ! 파이어 베이스 키도 등록해야하고, 클래스 기능구현도 덜 작성됨.

---
+++ MonkeyMacro/Database.cs
@@ -0,0 +1,48 @@
+﻿using Firebase.Database;
+using System.Threading.Tasks;
+
+public class Database
+{
+    private static Database _instance;
+    private FirebaseClient _client;
+
+    // Singleton 패턴을 사용하여 인스턴스를 하나만 유지
+    public static Database Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = new Database();
+            }
+            return _instance;
+        }
+    }
+
+    // Firebase 클라이언트 초기화
+    private Database()
+    {
+/*        _client = new FirebaseClient(
+            "https://your-database-url.firebaseio.com/",
+            new FirebaseOptions
+            {
+                AuthTokenAsyncFactory = () => Task.FromResult("your-firebase-database-secret")
+            });*/
+    }
+
+    // 데이터 가져오기
+    public async Task<T> GetDataAsync<T>(string path)
+    {
+        return await _client
+            .Child(path)
+            .OnceSingleAsync<T>();
+    }
+
+    // 데이터 쓰기
+    public async Task SetDataAsync<T>(string path, T data)
+    {
+        await _client
+            .Child(path)
+            .PutAsync(data);
+    }
+}
