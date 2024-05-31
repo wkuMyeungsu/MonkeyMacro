361791278a61ff516147d83433150808d9fa4de7
pms9158@wku.ac.kr <pms9158@wku.ac.kr>
2024-05-31 10:59:31 +0900

    ExampleApps 디렉토리 추가 : 앱에 들어가는 기능들을 예제로 올려놓음 - KeyCaptureApp 예제 추가 (단축키 기록 기능)

---
+++ ExampleApps/KeyCaptureApp/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+
+namespace KeyCaptureApp
+{
+    public partial class Form1 : Form
+    {
+        private StringBuilder keySequence;
+        private bool isCapturing;
+        private HashSet<Keys> pressedKeys;
+
+        public Form1()
+        {
+            InitializeComponent();
+            keySequence = new StringBuilder();
+            isCapturing = false;
+            pressedKeys = new HashSet<Keys>();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!isCapturing)
+            {
+                isCapturing = true;
+                button1.Text = "키 캡쳐 중단";
+                this.KeyPreview = true;
+                this.KeyDown += new KeyEventHandler(Form1_KeyDown);
+                this.KeyUp += new KeyEventHandler(Form1_KeyUp);
+            }
+            else
+            {
+                isCapturing = false;
+                button1.Text = "키 캡쳐 시작";
+                this.KeyPreview = false;
+                this.KeyDown -= new KeyEventHandler(Form1_KeyDown);
+                this.KeyUp -= new KeyEventHandler(Form1_KeyUp);
+                pressedKeys.Clear();
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (isCapturing && !pressedKeys.Contains(e.KeyCode))
+            {
+                pressedKeys.Add(e.KeyCode);
+                string key = GetKeyDisplayString(e.KeyCode);
+                keySequence.Append(key + " + ");
+                UpdateLabel();
+            }
+        }
+
+        private void UpdateLabel()
+        {
+            string displayText = keySequence.ToString();
+            if (displayText.EndsWith(" + "))
+            {
+                displayText = displayText.Substring(0, displayText.Length - 3);
+            }
+            label1.Text = displayText;
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (isCapturing)
+            {
+                pressedKeys.Remove(e.KeyCode);
+                keySequence.Clear();
+            }
+        }
+
+        private string GetKeyDisplayString(Keys key)
+        {
+            switch (key)
+            {
+                case Keys.ControlKey:
+                    return "Ctrl";
+                case Keys.ShiftKey:
+                    return "Shift";
+                case Keys.Menu:
+                    return "Alt";
+                case Keys.LWin:
+                case Keys.RWin:
+                    return "Win";
+                case Keys.Oemcomma:
+                    return "<";
+                case Keys.OemPeriod:
+                    return ">";
+                default:
+                    return key.ToString();
+            }
+        }
+    }
+}
